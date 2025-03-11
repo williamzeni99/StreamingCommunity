569b761e8dbd514e090cb773466937eca0e331aa
Lovi <62809003+Lovi-0@users.noreply.github.com>
2025-03-11 22:00:44 +0100

    pypi: Pep 625 adaptation

---
+++ StreamingCommunity/__init__.py
@@ -0,0 +1,6 @@
+# 11.03.25
+
+from .run import main
+from .Lib.Downloader.HLS import downloader as HLS_Downloader
+from .Lib.Downloader.MP4 import downloader as MP4_Downloader
+from .Lib.Downloader.TOR import downloader as TOR_Downloader
