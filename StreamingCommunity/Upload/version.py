c0f3d8619bb5ad2c04a02ee030b7d1cab06f6803
None <62809003+Arrowar@users.noreply.github.com>
2025-05-14 09:36:08 +0200

    Bump v3.0.6

--- StreamingCommunity/Upload/version.py
+++ StreamingCommunity/Upload/version.py
@@ -1,5 +1,5 @@
 __title__ = 'StreamingCommunity'
-__version__ = '3.0.5'
+__version__ = '3.0.6'
 __author__ = 'Arrowar'
 __description__ = 'A command-line program to download film'
 __copyright__ = 'Copyright 2024'
